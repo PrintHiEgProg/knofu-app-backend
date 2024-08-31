d2efdaa862a4f0a2ff3fad6a2c10f1631186b1cf
PrintHiEgProg <egorkobzev23gmail.com>
2024-08-31 20:13:13 +0300

    fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*",
+    origin: "https://conference-app-ten.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
