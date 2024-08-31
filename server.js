f5b78f172ceb3df9f7ef3260118814b6860129e0
PrintHiEgProg <egorkobzev23gmail.com>
2024-08-31 17:44:22 +0300

    vercel

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ io.on("connection", (socket) => {
 });
 
 // Запуск сервера
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
