b2d5d1bca8a81c2b440c467831a046b089768b62
PrintHiEgProg <egorkobzev23gmail.com>
2024-08-31 17:26:28 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  },
+});
+
+app.use(cors());
+app.use(express.json());
+
+const rooms = {};
+const conferences = {};
+
+app.post("/create-conference", (req, res) => {
+  const { roomID, conferenceName } = req.body;
+  conferences[roomID] = { name: conferenceName, admin: "Имя администратора" };
+  io.emit("new-conference", { roomID, conferenceName });
+  res.json({ message: "Конференция создана" });
+});
+
+// Обработка удаления конференции
+app.delete("/delete-conference/:roomID", (req, res) => {
+  const roomID = req.params.roomID;
+  if (conferences[roomID]) {
+    delete conferences[roomID];
+    io.emit("delete-conference", roomID);
+    res.json({ message: "Конференция удалена" });
+  } else {
+    res.status(404).json({ message: "Конференция не найдена" });
+  }
+});
+
+// Обработка подключения клиента
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  socket.emit("conferences", conferences);
+
+  //Chat
+  // Присоединение к комнате
+  socket.on("joinRoom", ({ roomID }) => {
+    socket.join(roomID);
+    console.log(`User joined room: ${roomID}`);
+
+    // Отправка сообщения о новом пользователе
+    socket
+      .to(roomID)
+      .emit("message", { user: "admin", text: `${socket.id} has joined!` });
+  });
+
+  // Обработка сообщений
+  socket.on("sendMessage", ({ roomID, message }) => {
+    io.to(roomID).emit("message", { user: socket.id, text: message });
+  });
+
+  // Обработка отключения
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+// Запуск сервера
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
