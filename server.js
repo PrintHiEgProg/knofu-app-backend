d47abe5fe3eff4fee8c7b3db42349b19b43f8113
PrintHiEgProg <egorkobzev23gmail.com>
2024-09-04 22:52:33 +0300

    webrtc deploy

--- server.js
+++ server.js
@@ -1,45 +1,121 @@
-const express = require("express");
-const cors = require("cors");
+const path = require('path');
+const express = require('express');
 const app = express();
-app.use(cors());
-const server = require("http").createServer(app);
-const io = require("socket.io")(server, {
-  cors: { origin: "https://knofu-app.vercel.app" },
+const socketIO = require('socket.io');
+
+const port = process.env.PORT || 8080;
+const env = process.env.NODE_ENV || 'development';
+
+// Redirect to https
+app.get('*', (req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https' && env !== 'development') {
+        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    next();
 });
 
-const PORT = 3000 || process.env.PORT;
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
-io.on("connection", (socket) => {
-  console.log("Connected");
-
-  // Generate a unique room ID for each client
-  const roomId = generateRoomId();
-  socket.join(roomId);
-
-  socket.on("message", (message) => {
-    // Broadcast messages only to clients in the same room
-    io.to(roomId).emit("message", message);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Disconnected");
-  });
+const server = require('http').createServer(app);
+server.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
 
-function generateRoomId() {
-  // Generate a unique room ID using a UUID library or a random string
-  return Math.random().toString(36).substr(2, 9);
-}
+/**
+ * Socket.io events
+ */
+const io = socketIO(server);
+io.sockets.on('connection', function (socket) {
+    /**
+     * Log actions to the client
+     */
+    function log() {
+        const array = ['Server:'];
+        array.push.apply(array, arguments);
+        socket.emit('log', array);
+    }
 
-function error(err, req, res, next) {
-  // log it
-  if (!test) console.error(err.stack);
+    /**
+     * Handle message from a client
+     * If toId is provided message will be sent ONLY to the client with that id
+     * If toId is NOT provided and room IS provided message will be broadcast to that room
+     * If NONE is provided message will be sent to all clients
+     */
+    socket.on('message', (message, toId = null, room = null) => {
+        log('Client ' + socket.id + ' said: ', message);
 
-  // respond with 500 "Internal Server Error".
-  res.status(500);
-  res.send("Internal Server Error");
-}
-app.use(error);
-server.listen(3000, () => {
-  console.log("listening on Port 3000");
+        if (toId) {
+            console.log('From ', socket.id, ' to ', toId, message.type);
+
+            io.to(toId).emit('message', message, socket.id);
+        } else if (room) {
+            console.log('From ', socket.id, ' to room: ', room, message.type);
+
+            socket.broadcast.to(room).emit('message', message, socket.id);
+        } else {
+            console.log('From ', socket.id, ' to everyone ', message.type);
+
+            socket.broadcast.emit('message', message, socket.id);
+        }
+    });
+
+    let roomAdmin; // save admins socket id (will get overwritten if new room gets created)
+
+    /**
+     * When room gets created or someone joins it
+     */
+    socket.on('create or join', (room) => {
+        log('Create or Join room: ' + room);
+
+        // Get number of clients in the room
+        const clientsInRoom = io.sockets.adapter.rooms.get(room);
+        let numClients = clientsInRoom ? clientsInRoom.size : 0;
+
+        if (numClients === 0) {
+            // Create room
+            socket.join(room);
+            roomAdmin = socket.id;
+            socket.emit('created', room, socket.id);
+        } else {
+            log('Client ' + socket.id + ' joined room ' + room);
+
+            // Join room
+            io.sockets.in(room).emit('join', room); // Notify users in room
+            socket.join(room);
+            io.to(socket.id).emit('joined', room, socket.id); // Notify client that they joined a room
+            io.sockets.in(room).emit('ready', socket.id); // Room is ready for creating connections
+        }
+    });
+
+    /**
+     * Kick participant from a call
+     */
+    socket.on('kickout', (socketId, room) => {
+        if (socket.id === roomAdmin) {
+            socket.broadcast.emit('kickout', socketId);
+            io.sockets.sockets.get(socketId).leave(room);
+        } else {
+            console.log('not an admin');
+        }
+    });
+
+    // participant leaves room
+    socket.on('leave room', (room) => {
+        socket.leave(room);
+        socket.emit('left room', room);
+        socket.broadcast.to(room).emit('message', { type: 'leave' }, socket.id);
+    });
+
+    /**
+     * When participant leaves notify other participants
+     */
+    socket.on('disconnecting', () => {
+        socket.rooms.forEach((room) => {
+            if (room === socket.id) return;
+            socket.broadcast
+                .to(room)
+                .emit('message', { type: 'leave' }, socket.id);
+        });
+    });
 });
